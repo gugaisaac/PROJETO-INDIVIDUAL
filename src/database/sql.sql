bf440afcd820cce3d6b6959fbbc7363e967e6ff5
gugaisaac <linosantanapereira@gmail.com>
2021-11-30 00:38:40 -0300

    site, front e back

---
+++ src/database/sql.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE the_tardis;
+
+USE the_tardis;
+
+CREATE TABLE usuario (
+	id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50)
+);
+
+CREATE TABLE aviso (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	titulo VARCHAR(100),
+    descricao VARCHAR(150),
+	fk_usuario INT,
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+); 
