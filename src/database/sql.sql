55d88c6b8a52771a9028416c74a6809d990aefce
aluno <aluno@bandtec.com>
2021-12-02 15:53:43 -0300

    telas e metrica arrumada

--- src/database/sql.sql
+++ src/database/sql.sql
@@ -3,16 +3,19 @@ CREATE DATABASE the_tardis;
 USE the_tardis;
 
 CREATE TABLE usuario (
-	id_usuario INT PRIMARY KEY AUTO_INCREMENT,
+	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
-	senha VARCHAR(50)
+	senha VARCHAR(50),
+    doutor_fav INT
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE Mural (
+	id_mural INT PRIMARY KEY AUTO_INCREMENT,
 	titulo VARCHAR(100),
     descricao VARCHAR(150),
 	fk_usuario INT,
 	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
 ); 
+SELECT count(doutor_fav) as 'qtd_doutor', doutor_fav as 'DoutorFav' FROM usuario group by doutor_fav order by doutor_fav ASC ;
+SELECT * FROM usuario;
